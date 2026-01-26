6da00de31e377f3a8934cf9a2108ae82c968af52
Mihai Hiticas <hiticasmihai7@gmail.com>
2026-01-26 18:39:40 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,31 @@
+require('dotenv').config()
+
+const express = require('express')
+const mongoose = require('mongoose')
+const workoutsRoutes = require('./routes/workouts')
+
+// express app
+const app = express()
+
+// middleware
+app.use(express.json())
+
+app.use((req, res, next) => {
+  console.log(`${req.path} ${req.method}`)
+  next()
+})
+
+// routes
+app.use('/api/workouts', workoutsRoutes)
+
+// connect to db
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    // listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log(`connect to db and listenning on http://localhost:${process.env.PORT}`)
+    })
+  })
+  .catch((error) => {
+    console.log(error)
+  })
