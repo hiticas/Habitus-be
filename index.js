2aedd4a8aedddd8d26edfd36356e5e2090ab7586
Mihai Hiticas <hiticasmihai7@gmail.com>
2026-01-28 17:02:09 +0200

    add user schema and jwt backend

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const workoutsRoutes = require('./routes/workouts')
+const userRoutes = require('./routes/user')
 
 const cors = require("cors");
 
@@ -32,6 +33,7 @@ app.use((req, res, next) => {
 
 // routes
 app.use('/api/workouts', workoutsRoutes)
+app.use('/api/user', userRoutes)
 
 // connect to db
 mongoose.connect(process.env.MONGO_URI)
